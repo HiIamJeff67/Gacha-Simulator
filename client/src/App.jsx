f8928931e34f8538d4dacc0a8ebff253ba727d48
HiIamJeff67 <iamjeffhi67@gmail.com>
2023-12-05 14:10:44 +0800

    v0.1.3 23-12-05

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
+import HomePage from './Pages/HomePage'
+import SimulatorFor1999 from './Pages/SimulatorFor1999'
+import SimulatorForGenshi from './Pages/SimulatorForGenshi'
+
 const App = () => {
   return (
     <Routes>
-        <Route index element={
-            <div>TEST</div>
+        <Route index element={  // Home Page
+           <HomePage/>
+        }/>
+        <Route path='/SimulatorFor1999' element={
+          <SimulatorFor1999/>
+        }/>
+        <Route path='/SimulatorForGenshi' element={
+          <SimulatorForGenshi/>
         }/>
     </Routes>
   )
