c693287aaed3495e72bdbbaecccd3883404353c1
HiIamJeff67 <iamjeffhi67@gmail.com>
2023-12-25 20:22:12 +0800

    v1.1.2 (ten summon not yet) 23-12-25

--- client/src/Pages/SimulatorFor1999/GachaingDisplay1999/MultiGachaDisplay1999.jsx
+++ client/src/Pages/SimulatorFor1999/GachaingDisplay1999/MultiGachaDisplay1999.jsx
@@ -1,9 +1,101 @@
+import { useState, useEffect } from 'react';
+import CharacterStarGenerator from './CharacterStarGenerator';
 import './MultiGachaDisplay1999.css';
 
+import UnilogImg from '../../../Images/unilog.png';
+import Star5Video from '../../../Videos/5-star.mp4';
+import testChrImg from '../../../Images/紅弩箭.png';
+
+const API_BASE = "http://localhost:8080";
+const _SUMMONTiMES = 10;
+
 const MultiGachaDisplay1999 = () => {
+  const [rerenderKey, setRerenderKey] = useState(0);  // to prevent the user fresh the page
+  const [tempChrInfo, setTempChrInfo] = useState([]);
+  const [chrInfo, setChrInfo] = useState([]);
+  const [videoState, setVideoState] = useState(false);
+
+  useEffect(() => {
+    setChrInfo(prevChrInfo => [...prevChrInfo, tempChrInfo]);
+  },[tempChrInfo])
+
+  useEffect(() => {
+    setTempChrInfo([]);
+    setChrInfo([]);
+    const abortController = new AbortController();
+
+    setVideoState(true);
+    for (let _ = 0; _ < _SUMMONTiMES; _++) {
+      getChrs(abortController);
+    }
+
+    return () => {
+      abortController.abort();
+    };
+  }, [rerenderKey]);
+
+  const getChrs = function (abortController) {
+    fetch(API_BASE + "/randomSelectOne", { signal: abortController.signal })
+      .then(res => res.json())
+      .then(data => {
+        setTempChrInfo([data.name, data.star, data.rateUp, data.rateStart, data.rateEnd]);
+      })
+      .catch(error => {
+        if (error === "AbortError") {
+          console.log(`Use Abort to debug twice fetching : ${error}`);
+        }
+        else {
+          console.log(`Error during summoning : ${error}`);
+        }
+      });
+  }
+
+  const summonAgain = function() {  // use useState to lead the useEffect to make a rerender
+    setRerenderKey(prevKey => prevKey + 1);
+  }
+
   return (
-    <div>MultiGachaDisplay1999</div>
+    <div className={`ten-summon-container-1999 ${videoState ? "" : "video-end"}`}>
+      <div className='result-container-1999'>
+        {videoState && (
+            <video 
+              className='video-1999'
+              autoPlay
+              onEnded={() => setVideoState(false)}
+              >
+              <source src={Star5Video} type='video/mp4'></source>
+            </video>)}
+        {!videoState && <div className='result-wrapper-1999'>
+          <div className='result-chr-wrapper-1999'>
+            {chrInfo.map((chrData, index) => {
+              if (chrData.length === 0) {
+                // console.log("This is for debugging");
+                return;
+              }
+              return (
+                <div className={`ten-result-chr-1999 
+                                 ${videoState ? "" : "active"} 
+                                 ${(index / ((_SUMMONTiMES / 2) + 1) < 1) ? "result-chr-top" : "result-chr-bottom"}`} 
+                     key={index}>
+                  <img className='ten-chr-img' src={testChrImg} alt=''></img>
+                  <div className='ten-chr-star'>
+                    <CharacterStarGenerator generateNum={chrData[1]}/>
+                  </div>
+                  <div className='ten-chr-name'>
+                    {chrData[0]}
+                  </div>
+                </div>)
+            })}
+            <div to={'/MultiGachaDisplay1999'} className='ten-draw-again-1999' onClick={summonAgain}>
+              <div className='ten-draw-again-icon'><img src={UnilogImg} width={'35px'} alt='unilog'/></div>
+              Summon x10
+            </div>
+          </div>
+        </div>}
+      </div>
+    </div>
   )
 }
+
 
 export default MultiGachaDisplay1999
